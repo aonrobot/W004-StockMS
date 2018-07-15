ee965722c2516bc73fb4bbb3ef9c1f567368c0e1
underdogdude <underdogbright@gmail.com>
2018-07-15 22:27:19 +0700

    add product with add product_catagory & product_branch

--- public/js/main.js
+++ public/js/main.js
@@ -52,7 +52,7 @@ $("#form_prod").submit(function (e) {
 
     event.preventDefault();
     var data = $('#form_prod').serializeArray();
-
+    console.log(data);
     data = {
         prodID: data[0].value,
         prodName: data[1].value,
@@ -82,3 +82,93 @@ $("#form_prod").submit(function (e) {
 function deleteRow(idx) {
     table.row(idx).remove().draw(false);
 }
+
+$('#prod_cat_modal').on('show.bs.modal', function (event) {
+        
+    var text_warning = $('#text_warning');
+    var catagory_name = $('#cat_code');
+    var catagory_list = $('#prod_cat');
+    
+    text_warning.empty();
+    catagory_name.val('');
+    if (catagory_name.hasClass('is-invalid')) {
+
+        catagory_name.removeClass('is-invalid');
+    }
+})
+
+$('#prod_branch_modal').on('show.bs.modal', function (event) {
+    
+    var warning = $('#text_warning_branch');
+    var branch_name = $('#branch_code');
+    var branch_list = $('#prod_branch');
+    
+    warning.empty();
+    branch_name.val('');
+
+    if (branch_name.hasClass('is-invalid')) {
+
+        branch_name.removeClass('is-invalid');
+    }
+})
+
+
+$('#addProdCat').click(function(e){ 
+    e.preventDefault();
+
+
+    var text_warning = $('#text_warning');
+    var catagory_name = $('#cat_code');
+    var catagory_list = $('#prod_cat');
+    
+    var catagory_name_value = catagory_name.val();
+    // clear
+    text_warning.empty();
+
+    // if user not type any data
+    if ( catagory_name.val().length === 0 ){
+
+        catagory_name.addClass('is-invalid');
+        text_warning.append('กรุณาใส่หมวดหมู่');
+
+    } else {
+        
+        catagory_list.append(`<option value="${ catagory_name_value }" selected="true"> ${ catagory_name_value } </option>`);
+
+        // Close Modal
+        $('#prod_cat_modal').modal('hide')
+
+    }       
+});
+
+$('#addBranch').click(function(e){ 
+    e.preventDefault();
+
+    var warning = $('#text_warning_branch');
+    var branch_name = $('#branch_code');
+    var branch_list = $('#prod_branch');
+    
+    var branch_name_value = branch_name.val();
+    // clear
+    warning.empty();
+
+    // if user not type any data
+    if ( branch_name.val().length === 0 ){
+
+        branch_name.addClass('is-invalid');
+        warning.append('กรุณาใส่คลังสินค้า');
+
+    } else {
+        
+        branch_list.append(`<option value="${ branch_name_value }" selected="true"> ${ branch_name_value } </option>`);
+
+        // Close Modal
+        $('#prod_branch_modal').modal('hide')
+
+    }       
+});
+
+$('#addProd').on('show.bs.modal', function (event) {
+    
+    $('#addProd').find('form')[0].reset();
+})
