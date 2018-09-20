c84cf43a436563775aa46af7e18f3e51a9cb1aaf
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2018-09-20 11:39:29 +0700

    fix travisci 9

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -37,7 +37,7 @@ abstract class DuskTestCase extends BaseTestCase
             ]);
 
             return RemoteWebDriver::create(
-                'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
+                'http://127.0.0.1:9222', DesiredCapabilities::chrome()->setCapability(
                     ChromeOptions::CAPABILITY, $options
                 )
             );
