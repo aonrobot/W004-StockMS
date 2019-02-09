7b29632821d3b394e1517ccb790ccf9073b480d9
Auttawut Wiriyakreng <aonrobotz@gmail.com>
2019-02-09 22:44:42 +0700

    change logout redirect

--- config/session.php
+++ config/session.php
@@ -148,7 +148,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'stockms.online'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
